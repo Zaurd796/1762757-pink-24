35d77a2d1083aee165f4c737ed56270cd42bc4e3
Zaur <zaurdysh@gmail.com>
2021-12-08 15:38:52 +0300

    Добавляет сетки на внутренние страницы

---
+++ source/js/tabs.js
@@ -0,0 +1,23 @@
+const uploadButtons = document.querySelectorAll(".upload__item-button");
+const contentBoxes = document.querySelectorAll(".upload__range-input-box");
+console.log(contentBoxes);
+
+uploadButtons.forEach(function (item) {
+  item.addEventListener("click", function() {
+    item.classList.toggle("upload__item-button--active");
+    contentBoxes.forEach(function (item) {
+      item.classList.add("upload__range-input-box--hidden");
+    });
+    const inputBox = document.querySelector("#" + this.dataset.tab);
+    inputBox.classList.remove("upload__range-input-box--hidden");
+  });
+});
+// uploadButtons.forEach(function(item) {
+//   item.addEventListener("click", function() {
+//     if (item !== this) {
+//       item.classList.remove("upload__item-button--active");
+//     }
+//     this.classList.toggle("upload__item-button--active");
+//     this.nextElementSibling.classList.toggle("upload__range-input--hidden");
+//   })
+// });
